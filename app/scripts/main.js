fa5a1112cff876f24c88b2e1d9fb204c92990dfc
Andrew <meilingandrew@gmail.com>
2016-05-12 22:56:13 -0700

    Add some todos - need to be done by 1pm Sat

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,17 @@ $(function() {
   var viewWindow = $(window),
       n          = $('nav');
 
+
+
   // this will handel the change in the navbar!
   viewWindow.scroll(function() {
 
-    if(viewWindow.scrollTop() !== 0) {
+    if(viewWindow.scrollTop() === 0) {
       // srink nav
-      n.removeClass('largeNav');
-
+      
     } else {
       // bigger nav
-      n.addClass('largeNav');
+      
     }
 
   });
